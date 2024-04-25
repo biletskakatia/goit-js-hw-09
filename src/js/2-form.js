813a7ac8a4e0524dff834ecab46fc04bb26b9bcb
biletskakatia <152877295+biletskakatia@users.noreply.github.com>
2024-04-25 23:01:48 +0300

    3-commit

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+let formData = { email: "", message: "" };
+const formRefs = {
+  form: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('input[name="email"]'),
+  messageInput: document.querySelector('textarea[name="message"]'),
+};
+const feedbackFormStateKey = 'feedback-form-state';
+formRefs.form.addEventListener('input', handleFormInput);
+formRefs.form.addEventListener('submit', handleSubmit);
+
+function handleFormInput(event) {
+    const { name, value } = event.target;
+  formData = { ...formData, [name]: value };
+  formDataToLocalStorage();
+}
+function formDataToLocalStorage() {
+  localStorage.setItem(feedbackFormStateKey, JSON.stringify(formData));
+}
+function loadFormDataFromLocalStorage() {
+  const savedFormData = localStorage.getItem(feedbackFormStateKey);
+  if (savedFormData) {
+    formData = JSON.parse(savedFormData);
+    updateFormInputs();
+  }
+}
+function updateFormInputs() {
+  formRefs.emailInput.value = formData.email;
+  formRefs.messageInput.value = formData.message;
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log('Form data:', formData);
+  clearFormDataAndLocalStorage();
+}
+function clearFormDataAndLocalStorage() {
+  formData = { email: "", message: "" };
+  localStorage.removeItem(feedbackFormStateKey);
+  updateFormInputs();
+}
+loadFormDataFromLocalStorage();
